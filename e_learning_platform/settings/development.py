56b47cc3966deb549810ef84392854ad4478dde5
Harendra Shah <sahh8645@gmail.com>
2026-07-21 15:09:27 +0545

    api link add

--- e_learning_platform/settings/development.py
+++ e_learning_platform/settings/development.py
@@ -6,7 +6,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
-    default="localhost,127.0.0.1,"
+    default="localhost,127.0.0.1,elearningbackend-ak5r.onrender.com"
 ).split(",")
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'elearningbackend-ak5r.onrender.com']
 
